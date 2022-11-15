4699a7806b880b94d4c7429e5e757183b6928792
Jaylson Almeida <85713598+Djaymam@users.noreply.github.com>
2022-11-15 22:54:41 -0100

    add database file

---
+++ database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS public.user_info
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying COLLATE pg_catalog."default" NOT NULL,
+    user_name character varying COLLATE pg_catalog."default" NOT NULL,
+    email character varying COLLATE pg_catalog."default" NOT NULL,
+    password character varying COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT user_info_pkey PRIMARY KEY (id),
+    CONSTRAINT unique_email UNIQUE (email),
+    CONSTRAINT unique_user_name UNIQUE (user_name)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.user_info
+    OWNER to postgres;
